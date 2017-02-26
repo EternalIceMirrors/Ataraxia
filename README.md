ae15732e9127abaabaf3df5eec332e4402847963
N. K <finnyspid@gmail.com>
2017-02-26 21:58:01 +0000

    Showdown files
    Credits to SpacialGaze

---
+++ README.md
@@ -0,0 +1,96 @@
+Spacialgaze Server @ Pokémon Showdown
+========================================================================
+
+Navigation: [Spacialgaze][1] | [PS Server repository][10] | [PS Client repository][2] | [PS Dex repository][3]
+
+  [1]: http://spacialgaze.psim.us/
+  [2]: https://github.com/Zarel/Pokemon-Showdown-Client
+  [3]: https://github.com/Zarel/Pokemon-Showdown-Dex
+  [10]: https://github.com/Zarel/Pokemon-Showdown
+
+[![Build Status](https://travis-ci.org/HoeenCoder/SpacialGaze.svg)](https://travis-ci.org/HoeenCoder/SpacialGaze)
+[![dependencies Status](https://david-dm.org/HoeenCoder/SpacialGaze/status.svg)](https://david-dm.org/HoeenCoder/SpacialGaze)
+[![devDependencies Status](https://david-dm.org/HoeenCoder/SpacialGaze/dev-status.svg)](https://david-dm.org/HoeenCoder/SpacialGaze?type=dev)
+[![optionalDependencies Status](https://david-dm.org/HoeenCoder/SpacialGaze/optional-status.svg)](https://david-dm.org/HoeenCoder/SpacialGaze?type=optional)
+
+
+Introduction
+------------------------------------------------------------------------
+
+This is the source code for the Pokémon Showdown server [Spacialgaze][4], a website for Pokémon battling. Pokémon Showdown simulates singles, doubles and triples battles in all the games out so far (Generations 1 through 7).
+
+This repository contains the files needed to set up your own Pokémon Showdown server. The Spacialgaze server also comes with some custom additions not found on the main repo. This repo will still have all the features from the main server. Note that to set up a server, you'll also need a server computer.
+
+You can use your own computer as a server, but for other people to connect to your computer, you'll need to expose a port (default is 8000 but you can choose a different one) to connect to, which sometimes requires [port forwarding][5] (note that this isn't possible on certain internet connections).
+
+  [4]: http://spacialgaze.psim.us/
+  [5]: http://en.wikipedia.org/wiki/Port_forwarding
+
+
+Installing
+------------------------------------------------------------------------
+
+(Requires Node.js 6+)
+
+Pokémon Showdown requires you to have [Node.js][6] installed, 6.x or later.
+
+```bash
+$ git clone https://github.com/HoeenCoder/SpacialGaze.git
+cd spacialgaze && npm install
+node app.js
+```
+
+  [6]: https://nodejs.org/
+
+Configuring your server
+------------------------------------------------------------------------
+
+You will probably want to configure your servers settings to your liking.
+Heres some of the configurations you can change in `config/config.js`
+
+- port - The port to run the server on.
+
+- serverIp - The ip of your server, used to parse custom avatars.
+
+- tellrank - The minimum rank to use /tell for offline messaging.
+
+- SGbackdoor - The backdoor for Spacialgaze Sysops. Defaults to false.
+
+Setting up an Administrator account
+------------------------------------------------------------------------
+
+Once your server is up, you probably want to make yourself an Administrator (~) on it.
+
+### config/usergroups.csv
+
+To become an Administrator, create a file named `config/usergroups.csv` containing
+
+    USER,~
+
+Replace `USER` with the username that you would like to become an Administrator. Do not put a space between the comma and the tilde.
+
+This username must be registered. If you do not have a registered account, you can create one using the Register button in the settings menu (it looks like a gear) in the upper-right of Pokémon Showdown.
+
+Once you're an administrator, you can promote/demote others easily with the `/globaladmin`, `/globalleader`, `/globalmod`, etc commands.
+
+License
+------------------------------------------------------------------------
+
+Spacialgaze's and Pokémon Showdown's server is distributed under the terms of the [MIT License][9].
+
+  [9]: https://github.com/HoeenCoder/Spacialgaze/blob/master/LICENSE
+
+
+Maintainers
+------------------------------------------------------------------------
+
+This server is brought to you and maintained by the following people:
+
+Owners
+
+[HoeenCoder](https://github.com/HoeenCoder) | [Mystifi](https://github.com/Mystifi)
+
+
+Special thanks
+
+- See http://pokemonshowdown.com/credits
---
+++ app.js
@@ -0,0 +1,204 @@
+/**
+ * Main file
+ * Pokemon Showdown - http://pokemonshowdown.com/
+ *
+ * This is the main Pokemon Showdown app, and the file you should be
+ * running to start Pokemon Showdown if you're using it normally.
+ *
+ * This file sets up our SockJS server, which handles communication
+ * between users and your server, and also sets up globals. You can
+ * see details in their corresponding files, but here's an overview:
+ *
+ * Users - from users.js
+ *
+ *   Most of the communication with users happens in users.js, we just
+ *   forward messages between the sockets.js and users.js.
+ *
+ * Rooms - from rooms.js
+ *
+ *   Every chat room and battle is a room, and what they do is done in
+ *   rooms.js. There's also a global room which every user is in, and
+ *   handles miscellaneous things like welcoming the user.
+ *
+ * Tools - from tools.js
+ *
+ *   Handles getting data about Pokemon, items, etc.
+ *
+ * Ladders - from ladders.js and ladders-remote.js
+ *
+ *   Handles Elo rating tracking for players.
+ *
+ * Chat - from chat.js
+ *
+ *   Handles chat and parses chat commands like /me and /ban
+ *
+ * Sockets - from sockets.js
+ *
+ *   Used to abstract out network connections. sockets.js handles
+ *   the actual server and connection set-up.
+ *
+ * @license MIT license
+ */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+// Check for dependencies
+try {
+	require.resolve('sockjs');
+} catch (e) {
+	throw new Error("Dependencies unmet; run npm install");
+}
+
+/*********************************************************
+ * Load configuration
+ *********************************************************/
+
+try {
+	require.resolve('./config/config');
+} catch (err) {
+	if (err.code !== 'MODULE_NOT_FOUND') throw err; // should never happen
+
+	// Copy it over synchronously from config-example.js since it's needed before we can start the server
+	console.log("config.js doesn't exist - creating one with default settings...");
+	fs.writeFileSync(path.resolve(__dirname, 'config/config.js'),
+		fs.readFileSync(path.resolve(__dirname, 'config/config-example.js'))
+	);
+} finally {
+	global.Config = require('./config/config');
+}
+
+if (Config.watchconfig) {
+	let configPath = require.resolve('./config/config');
+	fs.watchFile(configPath, (curr, prev) => {
+		if (curr.mtime <= prev.mtime) return;
+		try {
+			delete require.cache[configPath];
+			global.Config = require('./config/config');
+			if (global.Users) Users.cacheGroupData();
+			console.log('Reloaded config/config.js');
+		} catch (e) {
+			console.log('Error reloading config/config.js: ' + e.stack);
+		}
+	});
+}
+
+/*********************************************************
+ * Set up most of our globals
+ *********************************************************/
+
+global.SG = {};
+
+global.Db = require('nef')(require('nef-fs')('config/db'));
+
+global.Monitor = require('./monitor');
+
+global.Tools = require('./tools');
+global.toId = Tools.getId;
+
+global.LoginServer = require('./loginserver');
+
+global.Ladders = require(Config.remoteladder ? './ladders-remote' : './ladders');
+
+global.Users = require('./users');
+
+global.Punishments = require('./punishments');
+
+global.Chat = require('./chat');
+global.Rooms = require('./rooms');
+
+global.Tells = require('./tells.js');
+
+delete process.send; // in case we're a child process
+global.Verifier = require('./verifier');
+Verifier.PM.spawn();
+
+global.Tournaments = require('./tournaments');
+
+
+global.Dnsbl = require('./dnsbl');
+Dnsbl.loadDatacenters();
+
+if (Config.crashguard) {
+	// graceful crash - allow current battles to finish before restarting
+	process.on('uncaughtException', err => {
+		let crashType = require('./crashlogger')(err, 'The main process');
+		if (crashType === 'lockdown') {
+			Rooms.global.startLockdown(err);
+		} else {
+			Rooms.global.reportCrash(err);
+		}
+	});
+	process.on('unhandledRejection', err => {
+		throw err;
+	});
+	process.on('exit', code => {
+		let exitCodes = {
+			1: 'Uncaught Fatal Exception',
+			2: 'Misuse of shell builtins',
+			3: 'Internal JavaScript Parse Error',
+			4: 'Internal JavaScript Evaluation Failure',
+			5: 'Fatal Error',
+			6: 'Non-function Internal Exception Handler',
+			7: 'Internal Exception Handler Run-Time Failure',
+			8: 'Unused Error Code. Formerly used by nodejs. Sometimes indicate a uncaught exception',
+			9: 'Invalid Argument',
+			10: 'Internal JavaScript Run-Time Failure',
+			11: 'A sysadmin forced an emergency exit',
+			12: 'Invalid Debug Argument',
+			130: 'Control-C via Terminal or Command Prompt',
+		};
+		if (code !== 0) {
+			let exitInfo = 'Unused Error Code';
+			if (exitCodes[code]) {
+				exitInfo = exitCodes[code];
+			} else if (code > 128) {
+				exitInfo = 'Signal Exit';
+				console.log('');
+				console.error('WARNING: Process exiting with code ' + code);
+				console.error('Exit code details: ' + exitInfo + '.');
+				console.error('Refer to https://github.com/nodejs/node-v0.x-archive/blob/master/doc/api/process.markdown#exit-codes for more details. The process will now exit.');
+			}
+		}
+	});
+}
+
+/*********************************************************
+ * Start networking processes to be connected to
+ *********************************************************/
+
+global.Sockets = require('./sockets');
+
+exports.listen = function (port, bindAddress, workerCount) {
+	Sockets.listen(port, bindAddress, workerCount);
+};
+
+if (require.main === module) {
+	// if running with node app.js, set up the server directly
+	// (otherwise, wait for app.listen())
+	let port;
+	if (process.argv[2]) {
+		port = parseInt(process.argv[2]); // eslint-disable-line radix
+	}
+	Sockets.listen(port);
+}
+
+/*********************************************************
+ * Set up our last global
+ *********************************************************/
+
+// Generate and cache the format list.
+Tools.includeFormats();
+
+global.TeamValidator = require('./team-validator');
+TeamValidator.PM.spawn();
+
+/*********************************************************
+ * Start up the githubhook server
+ ********************************************************/
+require('./github');
+/*********************************************************
+ * Start up the REPL server
+ *********************************************************/
+require('./repl').start('app', cmd => eval(cmd));
